5f397e3fe0d914ab733aa61dee0fe46a80e09453
Peter Halverson <pete@touchpro.local>
2016-11-21 14:15:14 -0700

    October CMS Build 382

--- octobercms/builder.sh
+++ octobercms/builder.sh
@@ -18,46 +18,25 @@ function buildnpush {
 }
 
 
-tags=("build.380-apache" "build.380-php7.0-apache" "php7.0-apache" "latest")
+tags=("build.382-apache" "build.382-php7.0-apache" "php7.0-apache" "latest")
 path="$workdir/php7.0/apache"
 file="$path/Dockerfile"
 buildnpush "${tags[@]}"
 
-tags=("build.380-fpm" "build.380-php7.0-fpm" "php7.0-fpm")
+tags=("build.382-fpm" "build.382-php7.0-fpm" "php7.0-fpm")
 path="$workdir/php7.0/fpm"
 file="$path/Dockerfile"
 buildnpush "${tags[@]}"
 
-tags=("build.380-php5.6-apache" "php5.6-apache")
+tags=("build.382-php5.6-apache" "php5.6-apache")
 path="$workdir/php5.6/apache"
 file="$path/Dockerfile"
 buildnpush "${tags[@]}"
 
-tags=("build.380-php5.6-fpm" "php5.6-fpm")
+tags=("build.382-php5.6-fpm" "php5.6-fpm")
 path="$workdir/php5.6/fpm"
 file="$path/Dockerfile"
 buildnpush "${tags[@]}"
-
-#Edge builds
-# tags=("build.377-apache" "build.377-php7.0-apache" "edge")
-# path="$workdir/php7.0/apache"
-# file="$path/Dockerfile.edge"
-# buildnpush "${tags[@]}"
-#
-# tags=("build.377-fpm" "build.377-php7.0-fpm")
-# path="$workdir/php7.0/fpm"
-# file="$path/Dockerfile.edge"
-# buildnpush "${tags[@]}"
-#
-# tags=("build.377-php5.6-apache")
-# path="$workdir/php5.6/apache"
-# file="$path/Dockerfile.edge"
-# buildnpush "${tags[@]}"
-#
-# tags=("build.377-php5.6-fpm")
-# path="$workdir/php5.6/fpm"
-# file="$path/Dockerfile.edge"
-# buildnpush "${tags[@]}"
 
 #audit
 docker images aspendigital/octobercms | awk '{print $1","$2","$3 }' > audit.csv
