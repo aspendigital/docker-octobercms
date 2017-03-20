cabbb4dd9b5e955adea0b269e24d71f54539b273
Peter Halverson <pete@aspendigital.com>
2017-03-20 15:46:30 -0600

    Reintroduce edge build

--- update.sh
+++ update.sh
@@ -11,7 +11,7 @@ if ! hash sha1sum 2>&-; then { if ! hash openssl 2>&-; then echo "Error: openssl
 ##############
 ### Functions
 
-function check_october() {
+function check_october {
   [ "$1" = "edge" ] && EDGE=1 || EDGE=0
 
   # Host server PHP version - https://github.com/octobercms/october/blob/97b0bc481f948045f96a420bb54ab48628bfdddc/modules/system/classes/UpdateManager.php#L835
@@ -19,37 +19,42 @@ function check_october() {
 
   # Set default NULL HASH if core hash isn't set
   [ -z "$OCTOBERCMS_CORE_HASH" ] && OCTOBERCMS_CORE_HASH=6c3e226b4d4795d518ab341b0824ec29
+  [ -z "$OCTOBERCMS_EDGE_CORE_HASH" ] && OCTOBERCMS_EDGE_CORE_HASH=6c3e226b4d4795d518ab341b0824ec29
 
-  OCTOBER_API_RESPONSE=$(
-    curl -X POST -fsS --connect-timeout 15 --url http://gateway.octobercms.com/api/core/update \
-     -F "build=$OCTOBERCMS_BUILD" -F "core=$OCTOBERCMS_CORE_HASH" -F "plugins=a:0:{}" -F "server=$OCTOBERCMS_SERVER_HASH" -F "edge=$EDGE")
-  OCTOBER_API_UPDATES=$( echo "$OCTOBER_API_RESPONSE" | jq '. | { build: .core.build, hash: .core.hash, update: .update, updates: .core.updates }')
+  [ "$EDGE" -eq 1 ] && CORE_HASH="$OCTOBERCMS_EDGE_CORE_HASH" || CORE_HASH="$OCTOBERCMS_CORE_HASH";
+  [ "$EDGE" -eq 1 ] && CORE_BUILD="$OCTOBERCMS_EDGE_BUILD" || CORE_BUILD="$OCTOBERCMS_BUILD";
+
+  curl -X POST -fsS --connect-timeout 15 --url http://gateway.octobercms.com/api/core/update \
+   -F "build=$CORE_BUILD" -F "core=$CORE_HASH" -F "plugins=a:0:{}" -F "server=$OCTOBERCMS_SERVER_HASH" -F "edge=$EDGE" \
+    | jq '. | { build: .core.build, hash: .core.hash, update: .update, updates: .core.updates }' || exit 1
 }
 
 function update_checksum {
-  echo " - Generating new checksum..."
   if [ -z "$1" ]; then
     echo "Error: Invalid tag. Aborting..." && exit 1;
   else
-    TAG=$1;
+    local TAG=$1;
   fi
-  LATEST_ARCHIVE="octobercms-$TAG.tar.gz"
-  curl -o $LATEST_ARCHIVE -fS#L --connect-timeout 15 https://codeload.github.com/octobercms/october/legacy.tar.gz/$TAG
+
+  local ARCHIVE="octobercms-$TAG.tar.gz"
+  curl -o $ARCHIVE -fS#L --connect-timeout 15 https://codeload.github.com/octobercms/october/legacy.tar.gz/$TAG || exit 1;
   if hash sha1sum 2>&-; then
-    LATEST_ARCHIVE_CHECKSUM=$(sha1sum $LATEST_ARCHIVE | awk '{print $1}')
+    sha1sum $ARCHIVE | awk '{print $1}'
   elif hash openssl 2>&-; then
-    LATEST_ARCHIVE_CHECKSUM=$(openssl sha1 $LATEST_ARCHIVE | awk '{print $2}')
+    openssl sha1 $ARCHIVE | awk '{print $2}'
   else
     echo "Error: Could not generate checksum. Aborting" && exit 1;
   fi
-  echo "     $TAG | $LATEST_ARCHIVE_CHECKSUM"
-  rm $LATEST_ARCHIVE
+  rm $ARCHIVE
 }
 
 function update_dockerfiles {
 
-  current_tag="v1.0.$STABLE_BUILD"
-  checksum=$STABLE_CHECKSUM
+  [ "$1" = "edge" ] && local current_tag="v1.0.$EDGE_BUILD" || local current_tag="v1.0.$STABLE_BUILD"
+  [ "$1" = "edge" ] && local checksum=$EDGE_CHECKSUM || local checksum=$STABLE_CHECKSUM
+  [ "$1" = "edge" ] && local hash=$EDGE_CORE_HASH || local hash=$STABLE_CORE_HASH
+  [ "$1" = "edge" ] && local build=$EDGE_BUILD || local build=$STABLE_BUILD
+  [ "$1" = "edge" ] && local ext=".edge" || local ext=""
 
 	phpVersions=( php*.*/ )
   phpVersions=( "${phpVersions[@]%/}" )
@@ -73,13 +78,13 @@ function update_dockerfiles {
 			sed \
 				-e 's!%%OCTOBERCMS_TAG%%!'"$current_tag"'!g' \
 				-e 's!%%OCTOBERCMS_CHECKSUM%%!'"$checksum"'!g' \
-				-e 's!%%OCTOBERCMS_CORE_HASH%%!'"$STABLE_CORE_HASH"'!g' \
-				-e 's!%%OCTOBERCMS_CORE_BUILD%%!'"$STABLE_BUILD"'!g' \
+				-e 's!%%OCTOBERCMS_CORE_HASH%%!'"$hash"'!g' \
+				-e 's!%%OCTOBERCMS_CORE_BUILD%%!'"$build"'!g' \
 				-e 's!%%PHP_VERSION%%!'"$phpVersion"'!g' \
 				-e 's!%%VARIANT%%!'"$variant"'!g' \
 				-e 's!%%VARIANT_EXTRAS%%!'"$extras"'!g' \
 				-e 's!%%CMD%%!'"$cmd"'!g' \
-				Dockerfile.template > "$dir/Dockerfile"
+				Dockerfile.template > "$dir/Dockerfile$ext"
 
   	done
   done
@@ -108,34 +113,55 @@ function update_buildtags {
   		dir="$phpVersion/$variant"
   		[ -f "$dir/Dockerfile" ] || continue
 
-  		fullVersion="$(cat "$dir/Dockerfile" | awk '$1 == "ENV" && $2 == "OCTOBERCMS_TAG" { print $3; exit }')"
-  		fullVersion=build.${fullVersion#*v1.0.}
+  		fullVersion="$(cat "$dir/Dockerfile" | awk '$1 == "ENV" && $2 == "OCTOBERCMS_CORE_BUILD" { print $3; exit }')"
+  		fullVersion=build.$fullVersion
 
-  		versionAliases=()
-  		versionAliases+=(
-  			$fullVersion
-  			latest
-  		)
+      versionAliases=()
+  		versionAliases+=( $fullVersion latest )
 
-  		phpVersionVariantAliases=( "${versionAliases[@]/%/-$phpVersion-$variant}" )
+      phpVersionVariantAliases=( "${versionAliases[@]/%/-$phpVersion-$variant}" )
   		phpVersionVariantAliases=( "${phpVersionVariantAliases[@]//latest-/}" )
 
-  		fullAliases=(
-  			"${phpVersionVariantAliases[@]}"
-  		)
+  		fullAliases=( "${phpVersionVariantAliases[@]}" )
 
   		if [ "$phpVersion" = "$defaultPhpVersion" ]; then
   			if [ "$variant" = "$defaultVariant" ]; then
   				fullAliases+=( "${versionAliases[@]}" )
   			fi
   		fi
+
+      # Build edge tags
+      edgeVersion="$(cat "$dir/Dockerfile.edge" | awk '$1 == "ENV" && $2 == "OCTOBERCMS_CORE_BUILD" { print $3; exit }')"
+      edgeVersion=build.$edgeVersion
+
+      edgeAliases=()
+      edgeAliases+=( $edgeVersion edge )
+
+      phpEdgeVersionVariantAliases=( "${edgeAliases[@]/%/-$phpVersion-$variant}" )
+      phpEdgeVersionVariantAliases=( "${phpEdgeVersionVariantAliases[@]//latest-/}" )
+
+      fullEdgeAliases=( "${phpEdgeVersionVariantAliases[@]}" )
+
+      if [ "$phpVersion" = "$defaultPhpVersion" ]; then
+  			if [ "$variant" = "$defaultVariant" ]; then
+  				fullEdgeAliases+=( "${edgeAliases[@]}" )
+  			fi
+  		fi
+
   		tagsMarkdown+="- $(join ', ' "${fullAliases[@]}"): [$dir/Dockerfile](https://github.com/aspendigital/docker-octobercms/blob/master/$dir/Dockerfile)\n"
+  		edgeTagsMarkdown+="- $(join ', ' "${fullEdgeAliases[@]}"): [$dir/Dockerfile.edge](https://github.com/aspendigital/docker-octobercms/blob/master/$dir/Dockerfile.edge)\n"
   	done
   done
 
 	# Recreate README.md
-	sed '/Supported Tags/q' README.md | sed -e "s/CMS Build [0-9]*/CMS Build $STABLE_BUILD/" | sed -e "s/CMS%20Build-[0-9]*/CMS%20Build-$STABLE_BUILD/" > README_TMP.md
+	sed '/Supported Tags/q' README.md \
+   | sed -e "s/CMS Build [0-9]*/CMS Build $STABLE_BUILD/" \
+   | sed -e "s/CMS%20Build-[0-9]*/CMS%20Build-$STABLE_BUILD/" \
+   | sed -e "s/Edge Build [0-9]*/Edge Build $EDGE_BUILD/" \
+   | sed -e "s/Edge%20Build-[0-9]*/Edge%20Build-$EDGE_BUILD/" > README_TMP.md
 	echo -e "\n${tagsMarkdown[*]}" >> README_TMP.md
+  echo -e "\n### Edge Build Tags" >> README_TMP.md
+  echo -e "\n${edgeTagsMarkdown[*]}" >> README_TMP.md
 	sed -n -e '/Quick Start/,$p' README.md >> README_TMP.md
 	mv README_TMP.md README.md
 }
@@ -143,9 +169,30 @@ function update_buildtags {
 function update_repo {
   # commit changes to repository
 	echo " - Committing changes to repo..."
-  git add php*/*/Dockerfile README.md version
-  git commit -m "Build $STABLE_BUILD" -m "Automated update"
-  git tag "build.$STABLE_BUILD"
+  git add php*/*/Dockerfile* README.md version
+
+  if [ "$STABLE_UPDATE" -eq 1 ] && [ "$EDGE_UPDATE" -eq 1 ]; then
+    git commit -m "Build $STABLE_BUILD / Edge Build $EDGE_BUILD" -m "Automated update"
+  elif [ "$STABLE_UPDATE" -eq 1 ]; then
+    git commit -m "Build $STABLE_BUILD" -m "Automated update"
+  elif [ "$EDGE_UPDATE" -eq 1 ]; then
+    git commit -m "Edge Build $EDGE_BUILD" -m "Automated update"
+  fi
+
+  if git rev-parse "build.$STABLE_BUILD" >/dev/null 2>&1; then
+    echo "    Tag 'build.$STABLE_BUILD' already exists..."
+  else
+    echo "    Setting tag 'build.$STABLE_BUILD'..."
+    git tag "build.$STABLE_BUILD"
+  fi
+
+  if git rev-parse "build.$EDGE_BUILD" >/dev/null 2>&1; then
+    echo "    Tag 'build.$EDGE_BUILD' already exists..."
+  else
+    echo "    Setting tag 'build.$EDGE_BUILD'..."
+    git tag "build.$EDGE_BUILD"
+  fi
+
   git push && git push --tags
 }
 
@@ -172,50 +219,67 @@ echo "Automat: `date`"
 
 
 echo " - Querying October CMS API for updates..."
-check_october
-
-if [ "$(echo "$OCTOBER_API_RESPONSE" | jq -r '. | .update')" == "0" ]; then
+STABLE_RESPONSE=$(check_october)
+if [ "$(echo "$STABLE_RESPONSE" | jq -r '. | .update')" == "0" ]; then
   STABLE_UPDATE=0
+  STABLE_BUILD=$OCTOBERCMS_BUILD
+  STABLE_CORE_HASH=$OCTOBERCMS_CORE_HASH
+  STABLE_CHECKSUM=$OCTOBERCMS_CHECKSUM
   echo "    No STABLE build updates ($OCTOBERCMS_BUILD)";
 else
-  STABLE_BUILD=$(echo "$OCTOBER_API_UPDATES" | jq -r '. | .build')
-  STABLE_CORE_HASH=$(echo "$OCTOBER_API_UPDATES" | jq -r '. | .hash')
   STABLE_UPDATE=1
+  STABLE_BUILD=$(echo "$STABLE_RESPONSE" | jq -r '.build')
+  STABLE_CORE_HASH=$(echo "$STABLE_RESPONSE" | jq -r '.hash')
   echo "    New STABLE build ($OCTOBERCMS_BUILD -> $STABLE_BUILD)";
   echo "     STABLE Build: $STABLE_BUILD"
   echo "     STABLE core hash: $STABLE_CORE_HASH"
+  echo " - Generating new checksum..."
+  STABLE_CHECKSUM=$(update_checksum "v1.0.$STABLE_BUILD")
+  echo "     GitHub Tag v1.0.$STABLE_BUILD | $STABLE_CHECKSUM"
+fi
+
+echo " - Querying October CMS API for EDGE updates..."
+EDGE_RESPONSE=$(check_october edge)
+if [ "$(echo "$EDGE_RESPONSE" | jq -r '. | .update')" == "0" ]; then
+  EDGE_UPDATE=0
+  EDGE_BUILD=$OCTOBERCMS_EDGE_BUILD
+  EDGE_CORE_HASH=$OCTOBERCMS_EDGE_CORE_HASH
+  EDGE_CHECKSUM=$OCTOBERCMS_EDGE_CHECKSUM
+  echo "    No EDGE build updates ($OCTOBERCMS_EDGE_BUILD)";
+else
+  EDGE_UPDATE=1
+  EDGE_BUILD=$(echo "$EDGE_RESPONSE" | jq -r '.build')
+  EDGE_CORE_HASH=$(echo "$EDGE_RESPONSE" | jq -r '.hash')
+  echo "    New EDGE build ($OCTOBERCMS_EDGE_BUILD -> $EDGE_BUILD)";
+  echo "     EDGE Build: $EDGE_BUILD"
+  echo "     EDGE core hash: $EDGE_CORE_HASH"
+  echo " - Generating new checksum..."
+  EDGE_CHECKSUM=$(update_checksum "v1.0.$EDGE_BUILD")
+  echo "     GitHub Tag v1.0.$EDGE_BUILD | $EDGE_CHECKSUM"
 fi
 
 echo " - Fetching GitHub repository for latest tag..."
 GITHUB_LATEST_TAG=$( curl -fsS --connect-timeout 15 https://api.github.com/repos/octobercms/october/tags | jq -r '.[0] | .name') || exit 1;
 [ -z "$GITHUB_LATEST_TAG" ] && exit 1 || echo "    Latest repo tag: $GITHUB_LATEST_TAG";
 
-if [ "$STABLE_UPDATE" -eq 1 ]; then
-  update_checksum "v1.0.$STABLE_BUILD"
-  STABLE_CHECKSUM=$LATEST_ARCHIVE_CHECKSUM
-else
-  STABLE_CHECKSUM=$OCTOBERCMS_CHECKSUM
-fi
+if [ "$STABLE_UPDATE" -eq 1 ] || [ "$EDGE_UPDATE" -eq 1 ]; then
+  echo " - Setting build values..."
+  echo "    OCTOBERCMS_BUILD: $STABLE_BUILD" && echo "OCTOBERCMS_BUILD=$STABLE_BUILD" > version
+  echo "    OCTOBERCMS_CORE_HASH: $STABLE_CORE_HASH" && echo "OCTOBERCMS_CORE_HASH=$STABLE_CORE_HASH" >> version
+  echo "    OCTOBERCMS_CHECKSUM: $STABLE_CHECKSUM" && echo "OCTOBERCMS_CHECKSUM=$STABLE_CHECKSUM" >> version
+  echo "    OCTOBERCMS_EDGE_BUILD: $EDGE_BUILD" && echo "OCTOBERCMS_EDGE_BUILD=$EDGE_BUILD" >> version
+  echo "    OCTOBERCMS_EDGE_CORE_HASH: $EDGE_CORE_HASH" && echo "OCTOBERCMS_EDGE_CORE_HASH=$EDGE_CORE_HASH" >> version
+  echo "    OCTOBERCMS_EDGE_CHECKSUM: $EDGE_CHECKSUM" && echo "OCTOBERCMS_EDGE_CHECKSUM=$EDGE_CHECKSUM" >> version
+  update_dockerfiles && update_dockerfiles edge
+  update_buildtags
+  [ "$TEST" ] && echo ' - Testing. No changes committed.' || update_repo
 
-if [ "$STABLE_UPDATE" -eq 1 ]; then
-  if [ -z "$STABLE_BUILD" ] || [ -z "$STABLE_CORE_HASH" ] || [ -z "$STABLE_CHECKSUM" ]; then
-    echo " - No STABLE build, core hash or checksum";
-  else
-    echo " - Setting new build values..."
-    echo "    OCTOBERCMS_BUILD: $STABLE_BUILD" && echo "OCTOBERCMS_BUILD=$STABLE_BUILD" > version
-    echo "    OCTOBERCMS_CORE_HASH: $STABLE_CORE_HASH" && echo "OCTOBERCMS_CORE_HASH=$STABLE_CORE_HASH" >> version
-    echo "    OCTOBERCMS_CHECKSUM: $STABLE_CHECKSUM" && echo "OCTOBERCMS_CHECKSUM=$STABLE_CHECKSUM" >> version
-    update_dockerfiles
-    update_buildtags
-    [ "$TEST" ] && echo ' - Testing. No changes committed.' || update_repo
-
-    if [ "$SLACK_WEBHOOK_URL" ]; then
-	    echo -n " - Posting update to Slack..."
-	    curl -X POST -fsS --connect-timeout 15 --data-urlencode "payload={
-		    'text': 'October CMS Build $STABLE_BUILD.',
-	    }" $SLACK_WEBHOOK_URL
-	    echo -e ""
-    fi
+  if [ "$SLACK_WEBHOOK_URL" ]; then
+    echo -n " - Posting update to Slack..."
+    curl -X POST -fsS --connect-timeout 15 --data-urlencode "payload={
+	    'text': 'October CMS Build $STABLE_BUILD | Edge Build $EDGE_BUILD',
+    }" $SLACK_WEBHOOK_URL
+    echo -e ""
   fi
 fi
 
