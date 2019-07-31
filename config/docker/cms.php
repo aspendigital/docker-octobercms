a693af3f543110aa31fb5b37b7f2411840de4b70
Peter Halverson <pete@aspendigital.com>
2019-07-31 17:24:55 -0600

    Reorder settings to match core config. Copy to variants

--- config/docker/cms.php
+++ config/docker/cms.php
@@ -7,12 +7,12 @@
 return [
     'activeTheme' => env('CMS_ACTIVE_THEME', 'demo'),
     'edgeUpdates' => env('CMS_EDGE_UPDATES', false),
-    'disableCoreUpdates' => env('CMS_DISABLE_CORE_UPDATES', true),
     'backendUri' => env('CMS_BACKEND_URI', 'backend'),
+    'backendForceSecure' => env('CMS_BACKEND_FORCE_SECURE', false),
     'backendTimezone' => env('TZ', 'UTC'),
     'backendSkin' => env('CMS_BACKEND_SKIN', 'Backend\Skins\Standard'),
+    'disableCoreUpdates' => env('CMS_DISABLE_CORE_UPDATES', true),
     'linkPolicy' => env('CMS_LINK_POLICY', 'detect'),
-    'backendForceSecure' => env('CMS_BACKEND_FORCE_SECURE', false),
     'defaultMask' => [
         'file' => env('CMS_DEFAULT_MASK_FILE', '664'),
         'folder' =>  env('CMS_DEFAULT_MASK_FOLDER', '775'),
